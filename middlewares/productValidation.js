c3b5f49e96eed28d52f19ad8356a9628f420f47e
kaioruan <kaio.ruan@souunit.com.br>
2022-08-11 20:38:32 -0300

    Feat: create middlewares validation sales

--- middlewares/productValidation.js
+++ middlewares/productValidation.js
@@ -11,4 +11,29 @@ const nameValidation = (req, res, next) => {
   next();
 };
 
-module.exports = nameValidation;
+const saleValidation = (req, res, next) => {
+  const sales = req.body;
+  for (let i = 0; i < sales.length; i += 1) {
+    if (!('quantity' in sales[i])) {
+      return res.status(400).json({ message: '"quantity" is required' });
+    }
+    if (!('productId' in sales[i])) {
+      return res.status(400).json({ message: '"productId" is required' });
+    }
+  }
+  next();
+};
+
+const quantitySaleValitation = (req, res, next) => {
+  const sales = req.body;
+  for (let i = 0; i < sales.length; i += 1) {
+    if (sales[i].quantity <= 0) {
+      return res
+        .status(422)
+        .json({ message: '"quantity" must be greater than or equal to 1' });
+    }
+  }
+  next();
+};
+
+module.exports = { nameValidation, saleValidation, quantitySaleValitation };
