099a2935b6500f535df59cd7f59910867c88a6c2
kaioruan <kaio.ruan@souunit.com.br>
2022-08-11 16:17:26 -0300

    Feat: create router products and products/:id

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const productsController = require('./controllers/productsControllers');
 
 const app = express();
+app.use(express.json());
+
+app.get('/products', productsController.getByProducts);
+app.get('/products/:id', productsController.getByProductsById);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
