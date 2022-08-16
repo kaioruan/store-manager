7acc88d60d5995a5b083223b3c44c9dd1c4a4494
kaioruan <kaio.ruan@souunit.com.br>
2022-08-16 13:51:44 -0300

    Feat: create service delete sales:id

--- services/salesServices.js
+++ services/salesServices.js
@@ -8,4 +8,16 @@ const getByProductsById = async (id) => salesModels.getByProductsById(id);
 
 const createSale = async (sale) => salesModels.createSale(sale);
 
-module.exports = { getBySales, getBySalesById, createSale, getByProductsById };
+const deleteSale = async (id) => {
+  const removeSale = await salesModels.deleteSale(id);
+  if (!removeSale) return null;
+  return removeSale;
+};
+
+module.exports = {
+  getBySales,
+  getBySalesById,
+  createSale,
+  getByProductsById,
+  deleteSale,
+};
